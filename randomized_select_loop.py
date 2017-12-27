d9aff60f890f87a3668d1153438cf0eb8d89c090
Tramac <1145955920@qq.com>
2017-12-26 22:15:46 -0500

    9.2-3随机选择算法的循环版本

---
+++ randomized_select_loop.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+import random
+
+
+def partition(A, p, r):
+    # 快速排序中的partition过程，实现了对数组A[p, r]的原址重排
+    x = A[r - 1]
+    i = p - 1
+    repetition = 0
+    for j in range(p, r):
+        # 记录与A[r]相等的数目
+        if A[j - 1] == x:
+            repetition += 1
+        if A[j - 1] <= x:
+            i = i + 1
+            temp = A[i - 1]
+            A[i - 1] = A[j - 1]
+            A[j - 1] = temp
+    temp = A[i]
+    A[i] = A[r - 1]
+    A[r - 1] = temp
+    return i + 1 - repetition // 2
+
+
+def randomized_partition(A, p, r):
+    # 在A[p, r]中随机抽取一个元素与最后一个元素交换
+    i = random.randint(p, r)
+    A[r - 1], A[i - 1] = A[i - 1], A[r - 1]
+    return partition(A, p, r)  # 即返回一个新的A的partition过程
+
+
+def randomized_select_loop(A, p, r, i):
+    # 随机选择算法,返回数组A[p..r]中第i小的元素
+    while (p < r):
+        q = randomized_partition(A, p, r)
+        k = q - p + 1
+        if i == k:
+            return A[q - 1]
+        elif i < k:
+            r = q  # 改变上限
+        else:
+            p = q + 1  # 改变下限
+            i = i - k  # 改变顺序量
+    return A[p - 1]
+
+
+A = [4, 2, 7, 1, 5, 8, 6, 3]
+print(randomized_select_loop(A, 1, 8, 3))
