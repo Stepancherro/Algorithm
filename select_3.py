87fecd537de151a34f2f330dc4f384bb9bbedeca
Tramac <1145955920@qq.com>
2017-12-28 01:49:07 -0500

    9.3-5任意顺序统计量的选择问题算法

---
+++ select_3.py
@@ -0,0 +1,76 @@
+# -*- encoding: utf-8 -*-
+
+
+# 求解中位数的“黑箱子”子程序
+def median(items):
+    # 输入数组，返回中位数
+
+    # 中位数索引
+    def median_index(n):
+        if n % 2:  # 奇数情况
+            return n // 2
+        else:  # 偶数情况
+            return n // 2 - 1
+
+    def partition(items, element):
+        i = 0
+        for j in range(len(items)):
+            # 不需要先查找到element与最后一个元素交换, 遇到再做交换
+            if items[j] == element:
+                items[j], items[-1] = items[-1], items[j]
+
+            if items[j] < element:
+                items[i], items[j] = items[j], items[i]
+                i += 1
+
+        # 最后将element放到i处
+        items[i], items[-1] = items[-1], items[i]
+
+        return i + 1
+
+    def select(items, n):
+        # 如果数组只含1个元素,则返回该元素
+        if len(items) <= 1:
+            return items[0]
+
+        # 存储中位数
+        medians = []
+
+        # 分组,每组5个元素
+        for i in range(0, len(items), 5):
+            group = sorted(items[i: i + 5])  # 对每组排序
+            items[i: i + 5] = group  # 将已经排序好的子数组赋给原数组
+            median = group[median_index(len(group))]  # sorted() + median_index()
+            medians.append(median)
+
+        pivot = select(medians, median_index(len(medians)) + 1)  # 递归调用select(), 求解中位数的中位数
+        index = partition(items, pivot)  # 这里返回的是索引+1
+
+        if n == index:
+            return items[index - 1]
+        elif n < index:
+            return select(items[:index - 1], n)
+        else:
+            return select(items[index:], n - index)
+
+    return select(items[:], median_index(len(items)) + 1)
+
+
+# 利用求解中位数的“黑箱子”子程序解决任意顺序统计量的选择问题
+def select(items, n):
+    med = median(items)
+    # 根据中位数将原数组分为两部分
+    smaller = [item for item in items if item < med]
+    larger = [item for item in items if item > med]
+
+    # 判断n在哪个区间
+    if len(smaller) + 1 == n:
+        return med
+    elif len(smaller) + 1 > n:
+        return select(smaller, n)
+    else:
+        return select(larger, n - len(smaller) - 1)
+
+
+A = [4, 2, 7, 1, 5, 8, 6, 3, 28, 13, 22, 9, 15, 25, 21, 10, 11, 26, 18, 23, 19, 12, 14, 20, 16, 17, 24, 27]
+print(select(A, 25))
